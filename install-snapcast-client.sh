aa028584ebad570c85385579d654c0b050c396ca
Julius <77805198+Arcadia197@users.noreply.github.com>
2022-06-13 12:32:23 +0200

    Delete install-snapcast-client.sh
    overriding with executable file

--- install-snapcast-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-echo -n "Do you want to install Snapcast client (snapclient})? [y/N] "
-read REPLY
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 1; fi
-
-apt install --no-install-recommends -y snapclient
